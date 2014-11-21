abdc83ce1077f7456a7a2a3f4f8d904e333e0268
nishida <nishida@ngc224.org>
2014-11-21 20:28:53 +0900

    bugfix

--- toever.rb
+++ toever.rb
@@ -3,9 +3,9 @@ require "formula"
 HOMEBREW_PT_VERSION='1.8.1'
 class Toever < Formula
   homepage "https://github.com/ngc224/toever"
-  url "https://pypi.python.org/packages/source/t/toever/toever-1.8.1.tar.gz", :tag => "v#{VERSION}"
+  url "https://github.com/ngc224/toever.git", :tag => "v#{VERSION}"
   version VERSION
-  sha1 "f48da081a9082ac13e9764e4b539210757bcad0e"
+  sha1 "7860fb241a1f52e94ccf1b3d5e774dd13aba99ca"
   head "https://github.com/ngc224/toever.git", :branch => "master"
 
   resource "evernote" do
